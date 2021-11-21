6720fe6b9fb0458a034486f6ecd14d8dca2599d5
KaueVolpe <70399701+KaueVolpe@users.noreply.github.com>
2021-11-21 17:01:13 -0300

    Tela de perfil 80% feita
    Tela de perfil 80% feita

--- Front/src/api.js
+++ Front/src/api.js
@@ -1,7 +1,7 @@
-// import axios from 'axios'
+import axios from 'axios'
 
-// const api = axios.create({
-//     baseURL : "sua url do mock api aqui"
-// });
+const api = axios.create({
+    baseURL : "sua url do mock api aqui"
+});
 
-// export default api;
+export default api;

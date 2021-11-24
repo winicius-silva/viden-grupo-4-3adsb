d18a1365950114d4a3ec3a4621ab104e6a9ed16f
Samuel Lopes Almeida <samuel.almeida@bandtec.com.br>
2021-11-24 08:43:25 -0300

    Tela de video 80%

--- Front/src/components/Footer.jsx
+++ Front/src/components/Footer.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import face from "../assets/img/facebook.png";
+import insta from "../assets/img/instagram.png";
+import twitter from "../assets/img/twitter.png";
+import yt from "../assets/img/youtube.png";
 
 export default function Footer(){
     return(
@@ -9,16 +13,22 @@ export default function Footer(){
                     <h2><b>Viden</b></h2>
                     <br />
                     <h4>Viden-Solutions S.A</h4>
+                    <br />
                     <h4>Nossas redes sociais:</h4>
+                    <br />
                     <div className="icons_social">
-                        <img src="" alt="" />
-                        <img src="" alt="" />
+                        <img src={face} alt="" />
+                        <img src={insta} alt="" />
+                        <img src={twitter} alt="" />
+                        <img src={yt} alt="" />
                     </div>
                 </div>
-                <div className="div_footer_left"></div>
+
+                <div className="div_footer_left">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3075.4049491979613!2d-46.66340274023784!3d-23.557902040563814!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce59d2b3fc1ab9%3A0xe212f067274c24c3!2sRua%20Haddock%20Lobo%2C%20586%20-%20Cerqueira%20C%C3%A9sar%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2001414-000!5e0!3m2!1spt-BR!2sbr!4v1637724721293!5m2!1spt-BR!2sbr" width="700" height="250" styles="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                </div>
                 <div className="text_middle_footer">Viden - 2021</div>
             </div>
-
         </>
     );
 }
--- Front/src/pages/VideoPlayer.jsx
+++ Front/src/pages/VideoPlayer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../assets/styles/VideoPlayer.css"
+import "../assets/styles/dashboard.css"
 import Perfil from "../assets/img/perfil-white.png";
 
 
@@ -17,8 +18,8 @@ function VideoPlayer() {
                         <h4> Meus cursos </h4>
                     </div>
 
-                    <div id="icon-perfil-dash">
-                        <img id="img-perfil-dash" src={Perfil} alt="" />
+                    <div id="icon-perfil">
+                        <img id="img-perfil" src={Perfil} alt="" />
                     </div>
                     
                     <div id="btn-sair">
@@ -29,27 +30,39 @@ function VideoPlayer() {
 
             <div className="container-video">
                 <div className="videoPlayer">
-                    <iframe src="https://www.youtube.com/embed/ae6w0-kZ3-M" title="Aula 1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe src="https://www.youtube.com/embed/WObC_2e0kZk?list=PLdsnXVqbHDUcrE56CH8sXaPF3TTqoBP2z" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>  
                 
                 <div className="aulas-right">
-                    {/* <div className={}>
-
+                    
+                    <div>
+                        <h3>Aula 1:</h3>
+                        <iframe src="https://www.youtube.com/embed/p9-WuJbVHHc?list=PLdsnXVqbHDUcrE56CH8sXaPF3TTqoBP2z" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
 
-                    <div className={}>
-
+                    <div>
+                        <h3>Aula 2:</h3>
+                        <iframe src="https://www.youtube.com/embed/XcTTajFENHI?list=PLdsnXVqbHDUcrE56CH8sXaPF3TTqoBP2z" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
 
-                    <div className={}>
-
+                    <div>
+                        <h3>Aula 3:</h3>
+                        <iframe src="https://www.youtube.com/embed/NwAvovzHRDU?list=PLdsnXVqbHDUcrE56CH8sXaPF3TTqoBP2z" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
 
-                    <div className={}>
-                        
-                    </div> */}
+                    <div>
+                        <h3>Aula 4:</h3>
+                        <iframe src="https://www.youtube.com/embed/O4CWVQLbi48?list=PLdsnXVqbHDUcrE56CH8sXaPF3TTqoBP2z" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
                 </div>
 
+            </div>
+
+            <div className="descricao-aulas">
+                <br />
+                <h2>Sobre esse curso:</h2>
+                <br />
+                <h4>Este curso como objetivo ensinar ao aluno os principais conceitos de programação, para que ele esteja preparado ao executar as principais atividades que o mercado exige.</h4>
             </div>
 
             

274df866505ebaa94df7a421ce8a63a81e4e78d1
Winicius Silva <winicius.silva@VALEMOBI.CORP@VMNOTE-139.valemobi.corp>
2022-05-03 20:27:47 -0300

    componentizando menu do final de tela

--- App/Viden2/app/src/main/java/com/example/viden/MeusCursosCurso.kt
+++ App/Viden2/app/src/main/java/com/example/viden/MeusCursosCurso.kt
@@ -5,11 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.example.viden.fragment.Menu
 
 class MeusCursosCurso : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_meus_cursos_curso)
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            add<Menu>(R.id.containerFragmentMenu)
+        }
     }
 
     fun irMeusCursos(view: View){
--- App/Viden2/app/src/main/java/com/example/viden/MeusDados.kt
+++ App/Viden2/app/src/main/java/com/example/viden/MeusDados.kt
@@ -5,11 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.example.viden.fragment.Menu
 
 class MeusDados : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_meus_dados)
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            add<Menu>(R.id.containerFragmentMenu)
+        }
     }
 
     fun irMinhaConta(view: View){

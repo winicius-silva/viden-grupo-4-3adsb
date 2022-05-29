e8fedb7c8f85a609c939b60ee692de3783f8d3e4
Jorge Uliam de Lima <70400260+JorgeUL@users.noreply.github.com>
2022-05-29 11:44:53 -0300

    Salvando login do usuario

--- App/Viden2/app/src/main/java/com/example/viden/MainActivity.kt
+++ App/Viden2/app/src/main/java/com/example/viden/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.viden
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,6 +10,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val prefs = getSharedPreferences("USER", Context.MODE_PRIVATE)
+        if (prefs != null) {
+            startActivity(Intent(baseContext, MeusCursos::class.java))
+        }
     }
 
     fun entrar(view: View){
